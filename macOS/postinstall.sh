6d38b2441bb4d0a648524dad270c67ba94e07eeb
James Martindale <11380394+jkmartindale@users.noreply.github.com>
2021-10-21 16:52:38 -0500

    Update computer name

--- macOS/postinstall.sh
+++ macOS/postinstall.sh
@@ -1,2 +1,7 @@
+sudo scutil --set ComputerName "$COMPUTER_NAME"
+sudo scutil --set HostName "$COMPUTER_NAME"
+sudo scutil --set LocalHostName "$COMPUTER_NAME"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName "$COMPUTER_NAME"
+
 echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish

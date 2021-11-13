9bd358ba93685c1df8c24905c0c4bbfe8baf2c94
James Martindale <11380394+jkmartindale@users.noreply.github.com>
2021-11-12 18:07:41 -0600

    More macOS packages

--- macOS/config.fish
+++ macOS/config.fish
@@ -11,5 +11,12 @@ if status is-interactive
     test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 end
 
+fish_add_path /usr/local/opt/curl/bin
+
+# curl
+set -gx CPPFLAGS -I/usr/local/opt/curl/include
+set -gx LDFLAGS -L/usr/local/opt/curl/lib
+set -gx PKG_CONFIG_PATH /usr/local/opt/curl/lib/pkgconfig
+
 alias finished='afplay /System/Library/Sounds/Morse.aiff'
 alias dock-spacer="defaults write com.apple.dock persistent-apps -array-add '{\"tile-type\"=\"spacer-tile\";}'killall Dock"

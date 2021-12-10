2f87b4873ba13d2e8de3a30311537d39dc301d04
James Martindale <11380394+jkmartindale@users.noreply.github.com>
2021-12-09 17:44:05 -0700

    Move to symlinked fish config over merged

--- macOS/config.fish
+++ macOS/config.fish
@@ -1,3 +1,6 @@
+set -gx REPOSITORIES $HOME/Repositories
+source $REPOSITORIES/dotfiles/config.fish
+
 if status is-interactive
     # Homebrew
     if test -d (brew --prefix)"/share/fish/completions"
@@ -11,12 +14,11 @@ if status is-interactive
     test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 end
 
-fish_add_path /usr/local/opt/curl/bin
-
 # curl
+fish_add_path /usr/local/opt/curl/bin
 set -gx CPPFLAGS -I/usr/local/opt/curl/include
 set -gx LDFLAGS -L/usr/local/opt/curl/lib
 set -gx PKG_CONFIG_PATH /usr/local/opt/curl/lib/pkgconfig
 
-alias finished='afplay /System/Library/Sounds/Morse.aiff'
 alias dock-spacer="defaults write com.apple.dock persistent-apps -array-add '{\"tile-type\"=\"spacer-tile\";}'killall Dock"
+alias finished='afplay /System/Library/Sounds/Morse.aiff'
--- macOS/postinstall.sh
+++ macOS/postinstall.sh
@@ -8,3 +8,4 @@ chsh -s /usr/local/bin/fish
 
 sudo ln -s ~/Repositories/dotfiles/macOS/com.local.KeyRemapping.plist ~/Library/LaunchAgents/com.local.KeyRemapping.plist
 sudo chown root:wheel ~/Library/LaunchAgents/com.local.KeyRemapping.plist
+sudo ln -s ~/Repositories/dotfiles/macOS/config.fish ~/.config/fish/config.fish

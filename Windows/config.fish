a3183c8781371167c539493d24899675e28a971c
James Martindale <11380394+jkmartindale@users.noreply.github.com>
2023-04-03 11:55:56 -0700

    Fish: add Linuxbrew to path

--- Windows/config.fish
+++ Windows/config.fish
@@ -3,6 +3,8 @@ set -x SHELL fish
 
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
+fish_add_path ~/.local/bin
+
 # Unified config
 set -gx REPOSITORIES $USERPROFILE/Repositories
 source $REPOSITORIES/dotfiles/config.fish

6cbe5eebb2b13d3762fa924219dac887b263e999
James Martindale <11380394+jkmartindale@users.noreply.github.com>
2023-04-11 10:57:46 -0700

    Windows: fix ssh-agent

--- Windows/postinstall.ps1
+++ Windows/postinstall.ps1
@@ -1,9 +1,8 @@
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PowerShellGet -Force
-
 # TODO: fails until new shell started thanks to PowerShellGet upgrade
 Install-Module PSReadLine -AllowPrerelease -Force
 
-New-Item -ItemType HardLink -Path "C:\Users\JamesMartindale\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\ubuntu.png" -Value "C:\Users\JamesMartindale\Repositories\dotfiles\Windows\windows-terminal\ubuntu.png"
-
 New-Item -ItemType HardLink -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\ubuntu.png" -Value "$env:USERPROFILE\Repositories\dotfiles\Windows\windows-terminal\ubuntu.png"
+
+Set-Service -Name "ssh-agent" -StartupType "Automatic"

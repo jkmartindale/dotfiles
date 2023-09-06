04f2f71414ff4cf27c1e645c1c7b9f578c700a9d
James [C] Martindale <11380394+jkmartindale@users.noreply.github.com>
2023-09-06 14:16:03 -0700

    Fix WSL interop under systemd

--- Windows/config.fish
+++ Windows/config.fish
@@ -1,6 +1,13 @@
 # Variables
 set -x BROWSER wslview
 
+# Aliases/functions
+function wsl-interop -d "Re-enable WSL interop when systemd nukes it"
+    # https://github.com/microsoft/WSL/issues/8952#issuecomment-1568212651
+    sudo sh -c 'echo :WSLInterop:M::MZ::/init:PF > /usr/lib/binfmt.d/WSLInterop.conf'
+    sudo systemctl restart systemd-binfmt
+end
+
 # Homebrew
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 fish_add_path ~/.local/bin

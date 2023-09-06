69e804975f803e2c64dc480daaf28d87d1522cc5
James Martindale <11380394+jkmartindale@users.noreply.github.com>
2023-09-06 13:41:06 -0500

    ctrl+backspace for fish too

--- config.fish
+++ config.fish
@@ -13,4 +13,6 @@ if status is-interactive
     if test -d (brew --prefix)"/share/fish/vendor_completions.d"
         set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
     end
+
+    bind \cH backward-kill-word
 end

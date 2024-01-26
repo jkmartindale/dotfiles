252fd66b5c552e4ba9d41ec36485559877acf146
James Martindale <11380394+jkmartindale@users.noreply.github.com>
2024-01-26 14:51:56 -0800

    Fish: Early exit for non-interactive sessions

--- config.fish
+++ config.fish
@@ -1,23 +1,26 @@
 ssh-add -A 2>/dev/null
 
-if status is-interactive
-    # Variables
-    set -x EDITOR code
-    set -x SHELL (which fish)
+# Scripts etc. don't need all my laziness enablers
+if not status is-interactive
+    exit
+end
 
-    # Shell experience
-    starship init fish | source
-    bind \cH backward-kill-word
+# Variables
+set -x EDITOR code
+set -x SHELL (which fish)
 
-    # Aliases/functions
-    alias config="$EDITOR ~/.config/fish/config.fish"
-    alias brewlist='brew leaves | xargs brew deps --include-build --tree' # https://stackoverflow.com/a/61928483/3427178
+# Shell experience
+starship init fish | source
+bind \cH backward-kill-word
 
-    # Completions
-    if test -d (brew --prefix)"/share/fish/completions"
-        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
-    end
-    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
-    end
+# Aliases/functions
+alias config="$EDITOR ~/.config/fish/config.fish"
+alias brewlist='brew leaves | xargs brew deps --include-build --tree' # https://stackoverflow.com/a/61928483/3427178
+
+# Completions
+if test -d (brew --prefix)"/share/fish/completions"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+end
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
 end

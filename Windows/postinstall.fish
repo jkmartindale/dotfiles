28e4968259dd13d8b5d53a77732f32ed2746daeb
James [C] Martindale <11380394+jkmartindale@users.noreply.github.com>
2025-01-15 15:50:23 -0800

    [wsl] Set USERPROFILE

--- Windows/postinstall.fish
+++ Windows/postinstall.fish
@@ -1,2 +1,3 @@
+set WSLENV $WLSENV:USERPROFILE/p
 ln -s $USERPROFILE/Repositories/dotfiles/Windows/config.fish ~/.config/fish/config.fish
 ln -s $USERPROFILE/Repositories/dotfiles/starship.toml ~/.config/starship.toml

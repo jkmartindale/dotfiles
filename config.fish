c03da41967d233d0f8409a42d98afcfcef8901c3
James [C] Martindale <11380394+jkmartindale@users.noreply.github.com>
2025-02-03 14:23:44 -0800

    Add some missing fish split config

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
-eval (keychain --eval 2> /dev/null)
+#!/usr/bin/env fish
 
 # Scripts etc. don't need all my laziness enablers
 if not status is-interactive
@@ -10,8 +10,17 @@ set -x EDITOR code --wait
 set -x SHELL (which fish)
 
 # Shell experience
+eval (keychain --eval 2> /dev/null) # relies on SHELL variable for correct output
 starship init fish | source
 bind \cH backward-kill-word
+function fish_title
+    # So I don't get confused when I launch from PowerShell tab and the console theme is unchanged
+    echo [fish] (basename $PWD)
+end
+
+# Rust
+fish_add_path ~/.cargo/bin
+set -gx RUSTC_WRAPPER (which sccache)
 
 # Aliases/functions
 alias config="$EDITOR ~/.config/fish/config.fish"
@@ -23,4 +32,9 @@ if test -d (brew --prefix)"/share/fish/completions"
 end
 if test -d (brew --prefix)"/share/fish/vendor_completions.d"
     set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
+
+# Employer-specific config I don't want to publish
+if test -e ~/.config/fish/work.fish
+    source ~/.config/fish/work.fish
 end
